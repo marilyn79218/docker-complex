d4fb40cd644b3dffa00f3a4b7e6963cc6c8dc581
ray8863 <marilyn79218@gmail.com>
2020-01-30 00:33:06 +0800

    Detail explanation

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -5,6 +5,7 @@ import App from './App';
 it('renders without crashing', () => {
   // It will cause a creash as the App component is now attempts to render out the <Fib/>, 
   // where it'll make a request to our api-server, so we have to comment it out temporarily.
+  // See: https://www.udemy.com/course/docker-and-kubernetes-the-complete-guide/learn/lecture/11437334#overview
 
   // const div = document.createElement('div');
   // ReactDOM.render(<App />, div);
